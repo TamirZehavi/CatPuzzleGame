94dc3a68feba445ca152b1efe29871c198f49759
Tamir Zehavi (Cybercortex) <57259862+TamirZehavi@users.noreply.github.com>
2021-01-09 22:23:02 +0200

    first commit

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = door.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Vase")
+        {
+            
+            rigidBody.MovePosition(Vector2.up);
+            Debug.Log("it worked");
+        }
+    }
+
+
+}
